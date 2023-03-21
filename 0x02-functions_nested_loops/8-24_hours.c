ff98a280b30cc3888a0a91bb6a4948940982270b
Naledi Mosehla <u10246101@tuks.co.za>
2023-03-21 11:45:30 -0700

    0x02-functions_nested_loops projects updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints the minutes of a day
+ * jack_bauer - Prints every minute of a day
  *
- * Return: no return
+ * Return: Always 0
  */
+
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int h;
+	int m;
 
-	for (a = 48; a <= 50; a++)
+	for (h = 0; h < 24; h++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (m = 0; m < 60; m++)
 		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-						break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
 		}
-	}
+	} 
 }
