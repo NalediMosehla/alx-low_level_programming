896c1666715f61d87488a87fc97a1c60daeb7403
Naledi Mosehla <u10246101@tuks.co.za>
2023-03-23 10:39:49 -0700

    0x04-more_functions_nested_loops projects updated

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,14 +5,13 @@
  * @size: numbers of lines.
  * Return: no return.
  */
-
 void print_triangle(int size)
 {
 	int i, j;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < (size-1); j++)
+		for (j = 1; j < (size - i); j++)
 			_putchar(' ');
 		for (j--; j < size; j++)
 			_putchar(35);
